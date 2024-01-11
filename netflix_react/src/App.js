548accc9fd3bb98acca4aca2715d9f6e96f3a488
AAA <vdanielem@gmail.com>
2024-01-11 19:18:11 +0100

    Extra1

--- netflix_react/src/App.js
+++ netflix_react/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Navigazione from './components/Navigazione';
 import Section from './components/Section';
@@ -6,13 +7,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Mainsection from './components/Mainsection';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    // Cleanup function to clear the timeout when the component unmounts
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Navigazione />
-        <Section />
-        <Mainsection />
-        <Footer />
+        {loading ? (
+          <h1 className="text-danger text-center loader pulse">Loading...</h1> // Puoi sostituire questo con un componente loader più avanzato
+        ) : (
+          <>
+            <Navigazione />
+            <Section />
+            <Mainsection />
+            <Footer />
+          </>
+        )}
       </header>
     </div>
   );
--- netflix_react/src/components/Chiamata.jsx
+++ netflix_react/src/components/Chiamata.jsx
@@ -2,16 +2,31 @@ import React, { useState, useEffect } from 'react';
 
 const Chiamata = ({ nomedelfilm }) => {
   const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(`http://www.omdbapi.com/?apikey=c090253c&s=${nomedelfilm}`);
-      const data = await response.json();
-      console.log(data.Search);
-      setData(data.Search.slice(0, 6));
+      try {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=c090253c&s=${nomedelfilm}`);
+
+        if (!response.ok) {
+          throw new Error('Errore nella richiesta API');
+        }
+
+        const data = await response.json();
+        console.log(data.Search);
+        setData(data.Search.slice(0, 6));
+      } catch (error) {
+        setError(<p className="text-danger"> Si è verificato un errore durante il recupero dei dati. </p>);
+      }
     };
+
     fetchData();
   }, [nomedelfilm]);
+
+  if (error) {
+    return <div>Errore: {error}</div>;
+  }
 
   return (
     <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 no-gutters text-center">
