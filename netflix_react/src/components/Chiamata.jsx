e59087549f28dfe70ebaa55ca0d90c5978372f7d
AAA <vdanielem@gmail.com>
2024-01-12 09:31:42 +0100

    Da Hook a Class

--- netflix_react/src/components/Chiamata.jsx
+++ netflix_react/src/components/Chiamata.jsx
@@ -1,42 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const Chiamata = ({ nomedelfilm }) => {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://www.omdbapi.com/?apikey=c090253c&s=${nomedelfilm}`);
-
-        if (!response.ok) {
-          throw new Error('Errore nella richiesta API');
-        }
-
-        const data = await response.json();
-        console.log(data.Search);
-        setData(data.Search.slice(0, 6));
-      } catch (error) {
-        setError(<p className="text-danger"> Si è verificato un errore durante il recupero dei dati. </p>);
-      }
+class Chiamata extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      error: null
     };
-
-    fetchData();
-  }, [nomedelfilm]);
-
-  if (error) {
-    return <div>Errore: {error}</div>;
   }
 
-  return (
-    <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 no-gutters text-center">
-      {data && data.map((item) => (
-        <div key={item.imdbID}>
-          <img className="col mb-2 px-1 img-fluid" src={item.Poster} alt="movie picture" />
-        </div>
-      ))}
-    </div>
-  );
-};
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.nomedelfilm !== prevProps.nomedelfilm) {
+      this.fetchData();
+    }
+  }
+
+  fetchData = async () => {
+    try {
+      const response = await fetch(`http://www.omdbapi.com/?apikey=c090253c&s=${this.props.nomedelfilm}`);
+
+      if (!response.ok) {
+        throw new Error('Errore nella richiesta API');
+      }
+
+      const data = await response.json();
+      console.log(data.Search);
+      this.setState({ data: data.Search.slice(0, 6) });
+    } catch (error) {
+      this.setState({ error: <p className="text-danger"> Si è verificato un errore durante il recupero dei dati. </p> });
+    }
+  };
+
+  render() {
+    const { data, error } = this.state;
+
+    if (error) {
+      return <div>Errore: {error}</div>;
+    }
+
+    return (
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 no-gutters text-center">
+        {data && data.map((item) => (
+          <div key={item.imdbID}>
+            <img className="col mb-2 px-1 img-fluid" src={item.Poster} alt="movie picture" />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Chiamata;
